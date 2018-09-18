2a577c883f66e0d976e20950658df0f5da814f26
Paulina Collaguazo <paulina.nc.1419@gmail.com>
2018-09-18 17:55:39 -0500

    Segunda carga de archivos

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+import React from 'react';
+import ReactDOM from 'react-dom';
+//console.log("hola mundo")
+
+//aquí le  digo de  donde  buscarlo a la app
+const container = document.getElementById('app')
+
+//ReactDOM.render(que voy a renderizar, donde lo haré);
+const holaMundo= <h1>hola Estudiantes</h1>;
+ReactDOM.render(holaMundo,app);
